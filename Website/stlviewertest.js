8411c7b4ec248f0d43effe9d6b4bc390a26a73b5
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-06-26 13:03:14 -0400

    now works and has working orbit controls

--- Website/stlviewertest.js
+++ Website/stlviewertest.js
@@ -1,22 +1,35 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.132.2'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// three js docs https://threejs.org/docs/
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-});
+const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
 
-// SETUP
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const viewportSize = window.innerWidth /3;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( viewportSize, viewportSize ); // ame sure to also change ratio in camera
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+// orbit controls setup https://threejs.org/docs/?q=orbit#examples/en/controls/OrbitControls
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.enableDamping = true;
+controls.dampingFactor = 1;
+controls.rotateSpeed = 0.75;
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-scene.add(pointLight);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-renderer.render(scene, camera);
+camera.position.z = 5;
+
+function animate() {
+    //cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    
+	controls.update();
+    
+	renderer.render( scene, camera );
+}
+
+renderer.setAnimationLoop( animate );
