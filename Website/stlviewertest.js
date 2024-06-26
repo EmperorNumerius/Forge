be5bb15fa688eb5728996488870469e588d83c84
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-06-26 12:09:30 -0400

    should work after i install liveserver

---
+++ Website/stlviewertest.js
@@ -0,0 +1,22 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
+
+// SETUP
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+renderer.render(scene, camera);
