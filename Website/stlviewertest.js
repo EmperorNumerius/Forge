0ea1992f1801db4c20d26005cc41328f6072a5ac
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-06-26 21:32:01 -0400

    works

--- Website/stlviewertest.js
+++ Website/stlviewertest.js
@@ -1,31 +1,67 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+
 // three js docs https://threejs.org/docs/
 
+// camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
-
 const viewportSize = window.innerWidth /3;
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( viewportSize, viewportSize ); // ame sure to also change ratio in camera
+renderer.setSize( viewportSize, viewportSize ); // make sure to also change ratio in camera
 document.body.appendChild( renderer.domElement );
+camera.position.set( 20,20,20 );
+
 
 // orbit controls setup https://threejs.org/docs/?q=orbit#examples/en/controls/OrbitControls
 const controls = new OrbitControls( camera, renderer.domElement );
-controls.enableDamping = true;
-controls.dampingFactor = 1;
-controls.rotateSpeed = 0.75;
+controls.rotateSpeed = 1;
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+camera.position.set( 20, 20, 20 );
+
+// lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 2);
+directionalLight2.position.set(0, -10, -10);
+scene.add(directionalLight2);
+
+// background
+scene.background = new THREE.Color(0x666666);
+
+// guides
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
+
+// Binary files
+const loader = new STLLoader();
+
+const stlMaterial = new THREE.MeshPhongMaterial( { color: 0xAAAA00, specular: 0x111111, shininess: 200 } );
+
+loader.load( './models/test.stl', function ( geometry ) {
+
+    const mesh = new THREE.Mesh( geometry, stlMaterial );
+
+    mesh.position.set( 0, - 0.37, - 0.6 );
+    mesh.rotation.set( - Math.PI / 2, 0, 0 );
+    mesh.scale.set( 2, 2, 2 );
+
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    scene.add( mesh );
+
+} );
+
 
 camera.position.z = 5;
 
 function animate() {
-    //cube.rotation.y += 0.01;
-    //cube.rotation.x += 0.01;
     
 	controls.update();
     
