12c894a119a923dad40c210c3ee6d9e3bb6d2b5d
patcybermind <pato.gonzalezcabrera@gmail.com>
2024-07-28 19:14:09 -0400

    test

--- Website/stlViewer.js
+++ Website/stlViewer.js
@@ -7,7 +7,7 @@ import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 // camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 60, 1, 0.1, 1000 ); // ratio is 1 and first is fov  
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer(); 
 // Assuming there's a div with id="myDiv"
 const myDiv = document.getElementById('viewer');
 // Get the dimensions of the div
